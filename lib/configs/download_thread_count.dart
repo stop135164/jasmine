5a05a043206a92eba17e6510eb00eea21dcc72e7
stop135164 <155879351+stop135164@users.noreply.github.com>
2026-05-14 22:56:56 +0800

    Add files via upload

--- lib/configs/download_thread_count.dart
+++ lib/configs/download_thread_count.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:jasmine/basic/methods.dart';
 
 import '../basic/commons.dart';
-import 'is_pro.dart';
 
 late int _downloadThreadCount;
 int get downloadThreadCount => _downloadThreadCount;
@@ -18,12 +17,7 @@ Widget downloadThreadCountSetting() {
   return StatefulBuilder(
     builder: (BuildContext context, void Function(void Function()) setState) {
       return ListTile(
-        title: Text(
-          "下载线程数" + (!isPro ? "(发电)" : ""),
-          style: TextStyle(
-            color: !isPro ? Colors.grey : null,
-          ),
-        ),
+        title: const Text("下载线程数"),
         subtitle: Text("$_downloadThreadCount"),
         onTap: () async {
           await chooseDownloadThread(context);
@@ -35,11 +29,11 @@ Widget downloadThreadCountSetting() {
 }
 
 Future chooseDownloadThread(BuildContext context) async {
-  if (!isPro) {
-    defaultToast(context, "先发电才能使用多线程嗷");
-    return;
-  }
-  int? value = await chooseListDialog(context,title: "选择下载线程数",  values:_values,);
+  int? value = await chooseListDialog(
+    context,
+    title: "选择下载线程数",
+    values: _values,
+  );
   if (value != null) {
     await methods.set_download_thread(value);
     _downloadThreadCount = value;
